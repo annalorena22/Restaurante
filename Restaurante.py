bb4290b00bb6cbf0a4c1951938cd14761c11d631
annalorena22 <annalorena1998@gmail.com>
2024-08-27 16:17:57 -0400

    começando 🤪

--- Restaurante.py
+++ Restaurante.py
@@ -4,7 +4,9 @@ import customtkinter
 janelao = customtkinter.CTk()
 janelao.configure(bg='#2E2E2E')
 janelao.title("Restaurante do Ederson")
-janelao.geometry("1024x720")
+janelao.geometry("1080x1024")
+janelao.maxsize(1080,1024)
+janelao.minsize(1080,1024)
 
 def quadro(width, height, cor, posx, posy):
     customtkinter.CTkFrame(
@@ -14,21 +16,52 @@ def quadro(width, height, cor, posx, posy):
         fg_color=cor
         ).place(relx= posx, rely=posy, anchor="center")
     
-quadro(600, 700, "#ffdede", 0.5,0.5)
+quadro(1000, 820, "#FF6E40", 0.5,0.5)
 
-def texto(texto, cor_do_texto, fonte, tamanho, cor, posx, posy):
+def texto(texto, cor_do_texto, fonte, tamanho, cor, posx, posy, tipo_letra):
     customtkinter.CTkLabel(
         master=janelao,
         text = texto,
         text_color = cor_do_texto,
-        font = (fonte, tamanho),
+        font = (fonte, tamanho, tipo_letra),
         fg_color= cor
         ).place(relx= posx, rely=posy, anchor="center")
     
-texto("Restaurante do Éderson", "black", ("Arial bold"), 20, "#ffdede", 0.5, 0.1)
-texto("Efetue o login para realizar seu pedido:", "black", ("Arial"), 15, "#ffdede", 0.34, 0.2)
-texto("Login:", "black", ("Arial"), 15, "#ffdede", 0.34, 0.3)
-texto("Senha:", "black", ("Arial"), 15, "#ffdede", 0.34, 0.4)
+texto("Restaurante do Éderson", "white", ("Arial"), 35, "bold", "#FF6E40", 0.5, 0.2)
+texto("Efetue o login para realizar seu pedido:", "black", ("Arial"), 15, "normal", "#FF6E40", 0.8, 0.33)
+
+texto("Login:", "black", ("Arial"), 15, "bold", "#FF6E40", 0.7, 0.4)
+entrada_login = customtkinter.CTkEntry(
+    janelao, 
+    width=200,
+    height=30,
+    fg_color= "#FF8A65",
+    placeholder_text="Digite seu login",
+    placeholder_text_color= "#424242"
+    )
+entrada_login.place(relx=0.82, rely=0.4, anchor='center')
+
+texto("Senha:", "black", ("Arial"), 15, "bold", "#FF6E40", 0.7, 0.5)
+entrada_senha = customtkinter.CTkEntry(
+    janelao, 
+    width=200,
+    height=30,
+    fg_color= "#FF8A65",
+    placeholder_text="Digite sua senha",
+    placeholder_text_color= "#424242"
+    )
+entrada_senha.place(relx=0.82, rely=0.5, anchor='center')
+
+foto1 = customtkinter.CTkImage(
+    light_image=Image.open("hamburguer-inicio.jpg"),
+    size=(668,375)
+)
+foto1_label = customtkinter.CTkLabel(janelao, image=foto1, text="")
+foto1_label.place(relx=0.35, rely=0.5, anchor='center')
 
 
 janelao.mainloop()
+
+# arrumar cor da borda do entry;
+# arrumar negrito de alguns textos;
+# arrumar risquinho no login;
