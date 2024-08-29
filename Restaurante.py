d325b4c87d109f131c196a22944aa992e08378bb
annalorena22 <annalorena1998@gmail.com>
2024-08-29 17:25:45 -0400

    Meio errado neah

--- Restaurante.py
+++ Restaurante.py
@@ -8,18 +8,8 @@ janelao.title("Restaurante do Ederson")
 janelao.geometry("900x900")
 janelao.maxsize(900,900)
 janelao.minsize(900,900)
-janelao._set_appearance_mode("dark")
+customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
-
-#def pra trocar tela
-def trocar_janela():
-    tela_login.pack_forget()
-    tela_menu.pack(fill="both", expand = True)
-
-
-#frame Janela 1 = Login 
-tela_login = customtkinter.CTkFrame(janelao)
-tela_login.pack(fill="both", expand = True)
 
 #def pra gerar um quadrado com cor
 def quadro(master, width, height, cor, posx, posy):
@@ -37,29 +27,48 @@ def texto(master,texto, cor_do_texto, fonte, tamanho, cor, posx, posy):
         text = texto,
         text_color = cor_do_texto,
         font = (fonte, tamanho, "bold"),
-        fg_color= cor
+        bg_color= cor
         ).place(relx= posx, rely=posy, anchor="center")
+    
+#def pra janela de aviso personalizada    
+def janela_aviso(titulo, texto):
+    aviso = customtkinter.CTk()
+    master = tela_login
+    aviso.title(titulo)
+    aviso.geometry("300x150")
+    aviso.eval('tk::PlaceWindow . right')
+    aviso.resizable(False, False)
+    aviso.configure(bg_color="#272e36")
+    aviso_label = customtkinter.CTkLabel(aviso, text=texto, wraplength=250, text_color="white")
+    aviso_label.place(relx=0.5, rely=0.3, anchor='center')
+
+    def fechar_janela_aviso():
+        aviso.destroy()
+        tela_menu.pack(fill="both", expand=True)
+
+    botao_ok = customtkinter.CTkButton(aviso, text="OK", fg_color="#424f5c", hover_color="#0a6638", command=fechar_janela_aviso)
+    botao_ok.place(relx=0.5, rely=0.7, anchor='center')
+
+    aviso.mainloop()
 
 #def função botão "Entrar"
 def funcao_botao():
-    global entrada_login, entrada_senha, entrada_confirmar_senha
     login = entrada_login.get()
     senha = entrada_senha.get()
     senha_de_novo = entrada_confirmar_senha.get()
 
     if login == "" or senha == "" or senha_de_novo == "":
-        messagebox.showwarning("ATENÇÃO!", "Campo não preenchido, confira os campos de cadastro e tente novamente.")
+        janela_aviso("Aviso", "Campo não preenchido, confira os campos de cadastro e tente novamente.")
     elif login == senha:
-        messagebox.showwarning("ATENÇÃO!","O login e a senha nao podem ser iguais.")
+        janela_aviso("Aviso","O login e a senha não podem ser iguais.")
     elif senha != senha_de_novo:
-        messagebox.showwarning("ATENÇÃO!", "Senhas não coincidem!!")
+        janela_aviso("Aviso","Senhas não coincidem.")
     else:
-       messagebox.showwarning("SUCESSO!","Cadastro Realizado com Sucesso!")
-       trocar_janela()
+        janela_aviso("Aviso","Sucesso! Clique em 'OK' para visualizar o menu.")
+        tela_login.pack_forget()
 
-
-def botao_clicar():
-    funcao_botao()
+tela_login = customtkinter.CTkFrame(janelao)
+tela_login.pack(fill="both", expand = True)
 
 quadro(tela_login, 800, 720, "#272e36", 0.5,0.5)
 
@@ -74,7 +83,7 @@ texto(tela_login, "Éderson Restaurant", "white", ("Courier New"), 35, "#272e36"
 texto(tela_login, "Olá, seja bem-vindo(a)!", "white", ("Arial"), 15, "#272e36", 0.5, 0.28)
 texto(tela_login, "Efetue o login para realizar seu pedido:", "white", ("Arial"), 15, "#272e36", 0.5, 0.33)
 
-texto(tela_login, "Login:", "white", ("Arial"), 15, "#272e36", 0.35, 0.4)
+texto(tela_login, "Login:", "white", ("Arial"), 13, "#272e36", 0.35, 0.4)
 entrada_login = customtkinter.CTkEntry(
     tela_login, 
     width=200,
@@ -86,7 +95,7 @@ entrada_login = customtkinter.CTkEntry(
     )
 entrada_login.place(relx=0.55, rely=0.4, anchor='center')
 
-texto(tela_login,"Senha: ", "white", ("Arial"), 15, "#272e36", 0.35, 0.45)
+texto(tela_login,"Senha: ", "white", ("Arial"), 13, "#272e36", 0.35, 0.45)
 entrada_senha = customtkinter.CTkEntry(
     tela_login, 
     width=200,
@@ -99,8 +108,8 @@ entrada_senha = customtkinter.CTkEntry(
     )
 entrada_senha.place(relx=0.55, rely=0.45, anchor='center')
 
-texto(tela_login, "Confirme a", "white", ("Arial"), 14, "#272e36", 0.35, 0.5)
-texto(tela_login,"Senha:", "white", ("Arial"), 14, "#272e36", 0.35, 0.53)
+texto(tela_login, "Confirme a", "white", ("Arial"), 12, "#272e36", 0.35, 0.5)
+texto(tela_login,"Senha:", "white", ("Arial"), 12, "#272e36", 0.35, 0.53)
 entrada_confirmar_senha = customtkinter.CTkEntry(
     tela_login, 
     width=200,
@@ -111,7 +120,7 @@ entrada_confirmar_senha = customtkinter.CTkEntry(
     placeholder_text="Confirme a senha",
     placeholder_text_color= "#b6babf"
     )
-entrada_confirmar_senha.place(relx=0.55, rely=0.5, anchor='center')
+entrada_confirmar_senha.place(relx=0.55, rely=0.51, anchor='center')
 
 botao_cadastrar = customtkinter.CTkButton(
     tela_login,
@@ -119,11 +128,11 @@ botao_cadastrar = customtkinter.CTkButton(
     text_color = "white",
     font = ("Arial", 12, "bold"),
     fg_color= "#424f5c",
-    command= botao_clicar
-    ).place(relx= 0.5, rely=0.6, anchor="center")
+    command= funcao_botao
+    )
+botao_cadastrar.place(relx=0.5, rely=0.6, anchor="center")
 
 tela_menu = customtkinter.CTkFrame(janelao)
-
 quadro(tela_menu, 800, 720, "#272e36", 0.5, 0.5)
 texto(tela_menu, "Éderson Restaurant", "white", ("Courier New"), 35, "#272e36", 0.5, 0.16)
 
